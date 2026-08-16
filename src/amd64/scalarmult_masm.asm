78017958f0e196889d1a04339bf1150843a77946
tevador <tevador@gmail.com>
2026-08-16 16:41:51 +0200

    AMD64X impl: faster modular inversion

--- src/amd64/scalarmult_masm.asm
+++ src/amd64/scalarmult_masm.asm
@@ -1,4 +1,4 @@
-; Copyright (c) 2022 tevador <tevador@gmail.com>
+; Copyright (c) 2022-2026 tevador <tevador@gmail.com>
 ;
 ; This file is part of mx25519, which is released under LGPLv3.
 ; See LICENSE for full license details.
@@ -14,6 +14,9 @@ PUBLIC mx25519_scalarmult_amd64
 
 include constants.inc
 
+ALIGN 32
+include gcd_table.inc
+
 mx25519_scalarmult_amd64x PROC
   mov   qword ptr [rsp+8], rdi
   mov   qword ptr [rsp+16], rsi
@@ -22,9 +25,31 @@ mx25519_scalarmult_amd64x PROC
   mov   rdx, r8
   mov   rcx, r9
   movzx r8, byte ptr [rsp+40]
+  sub   rsp, 160
+  vmovdqu xmmword ptr [rsp+144], xmm6
+  vmovdqu xmmword ptr [rsp+128], xmm7
+  vmovdqu xmmword ptr [rsp+112], xmm8
+  vmovdqu xmmword ptr [rsp+96], xmm9
+  vmovdqu xmmword ptr [rsp+80], xmm10
+  vmovdqu xmmword ptr [rsp+64], xmm11
+  vmovdqu xmmword ptr [rsp+48], xmm12
+  vmovdqu xmmword ptr [rsp+32], xmm13
+  vmovdqu xmmword ptr [rsp+16], xmm14
+  vmovdqu xmmword ptr [rsp], xmm15
 
 include scalarmult_mulx_adx.inc
 
+  vmovdqu xmm15, xmmword ptr [rsp]
+  vmovdqu xmm14, xmmword ptr [rsp+16]
+  vmovdqu xmm13, xmmword ptr [rsp+32]
+  vmovdqu xmm12, xmmword ptr [rsp+48]
+  vmovdqu xmm11, xmmword ptr [rsp+64]
+  vmovdqu xmm10, xmmword ptr [rsp+80]
+  vmovdqu xmm9, xmmword ptr [rsp+96]
+  vmovdqu xmm8, xmmword ptr [rsp+112]
+  vmovdqu xmm7, xmmword ptr [rsp+128]
+  vmovdqu xmm6, xmmword ptr [rsp+144]
+  add   rsp, 160
   mov   rdi, qword ptr [rsp+8]
   mov   rsi, qword ptr [rsp+16]
   ret
